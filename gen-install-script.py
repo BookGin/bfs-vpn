2614c8858227affed5206b0bc2883c3c499c4426
bookgin <dongsheoil@gmail.com>
2018-08-01 23:46:09 +0800

    Fix typo

--- gen-install-script.py
+++ gen-install-script.py
@@ -11,15 +11,15 @@ MAC_TEMPLATE_FILE='templates/mac.mobileconfig.j2'
 WINDOWS_TEMPLATE_FILE='templates/windows.ps1.j2'
 LINUX_TEMPLATE_FILE='templates/linux.sh.j2'
 
-def generateMacConfig(username, hostname, p12_base64, cacert_base64, vpn_name):
-    with open(WINDOWS_TEMPLATE_FILE) as f:
+def generateMacConfig(username, hostname, p12_base64, cacert_base64, vpnname):
+    with open(MAC_TEMPLATE_FILE) as f:
         template = Template(f.read())
     return template.render(
         PayloadContentCA=cacert_base64,
         p12_base64=p12_base64,
         username=username,
         IP_subject_alt_name=hostname,
-        vpn_name=vpn_name,
+        vpnname=vpnname,
         uuid=str(uuid4()).upper(),
         pkcs12_PayloadCertificateUUID=str(uuid4()).upper(),
         VPN_PayloadIdentifier=str(uuid4()).upper(),
@@ -28,7 +28,7 @@ def generateMacConfig(username, hostname, p12_base64, cacert_base64, vpn_name):
         PayloadUUID=str(uuid4()).upper(), 
     )
 
-def generateWindowsConfig(username, hostname, p12_base64, cacert_base64, vpn_name):
+def generateWindowsConfig(username, hostname, p12_base64, cacert_base64, vpnname):
     with open(WINDOWS_TEMPLATE_FILE) as f:
         template = Template(f.read())
     return template.render(
@@ -36,17 +36,17 @@ def generateWindowsConfig(username, hostname, p12_base64, cacert_base64, vpn_nam
         p12_base64=p12_base64,
         username=username,
         IP_subject_alt_name=hostname,
-        vpn_name=vpn_name,
+        vpnname=vpnname,
     )
 
-def generateLinuxConfig(username, hostname, p12_cert, vpn_name):
+def generateLinuxConfig(username, hostname, p12_base64, vpnname):
     with open(LINUX_TEMPLATE_FILE) as f:
         template = Template(f.read())
     return template.render(
         username=username,
         hostname=hostname,
-        vpn_name=vpn_name,
-        p12_cert=p12_cert,
+        vpnname=vpnname,
+        p12_base64=p12_base64,
     )
 
 def parseArgv():
@@ -59,23 +59,22 @@ def parseArgv():
     return parser.parse_args()
 
 def main(argv):
-    with open(argv.p12) as f:
-        p12_cert = f.read()
-        p12_base64 = base64.b64encode(p12_cert).decode()
+    with open(argv.p12, 'rb') as f:
+        p12_base64 = base64.b64encode(f.read()).decode()
     with open(argv.cacert) as f:
-        cacert_base64 = base64.b64encode(f.read()).decode()
+        cacert_base64 = base64.b64encode(f.read().encode()).decode()
 
     os.mkdir(argv.username)
 
-    windows_config = generateWindoesConfig(argv.username, argv.hostname, p12_base64, cacert_base64, argv.vpnname)
-    with open(os.path.join(argv.username, argv.vpnname + "-windows.ps1"), "wb") as f:
+    windows_config = generateWindowsConfig(argv.username, argv.hostname, p12_base64, cacert_base64, argv.vpnname)
+    with open(os.path.join(argv.username, argv.vpnname + "-windows.ps1"), "w") as f:
         f.write(windows_config)
 
     mac_config = generateMacConfig(argv.username, argv.hostname, p12_base64, cacert_base64, argv.vpnname)
-    with open(os.path.join(argv.username, argv.vpnname + "-mac.mobileconfig"), "wb") as f:
+    with open(os.path.join(argv.username, argv.vpnname + "-mac.mobileconfig"), "w") as f:
         f.write(mac_config)
-    linux_config = generateLinuxConfig(argv.username, argv.hostname, p12_cert, argv.vpnname)
-    with open(os.path.join(argv.username, argv.vpnname + "-linux.sh"), "wb") as f:
+    linux_config = generateLinuxConfig(argv.username, argv.hostname, p12_base64, argv.vpnname)
+    with open(os.path.join(argv.username, argv.vpnname + "-linux.sh"), "w") as f:
         f.write(linux_config)
 
 if __name__ == '__main__':
