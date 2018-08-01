a77016154690271a083dd27f6d7ac9e8c75cb39b
bookgin <dongsheoil@gmail.com>
2018-08-01 23:58:04 +0800

    Add missing initial host cert script

--- init-host-cert.sh
+++ init-host-cert.sh
@@ -29,3 +29,47 @@ ipsec pki --pub --type ecdsa --in private/hostkey.pem \
 --san vpn.bfs.tw \
 --flag serverAuth --flag ikeIntermediate \
 > certs/vpn.bfs.tw.pem
+
+# Specify host key in the ipsec.secrets
+cat << EOF > /etc/ipsec.secrets
+# ipsec.secrets - strongSwan IPsec secrets file
+
+: ECDSA hostkey.pem
+EOF
+chmod 600 /etc/ipsec.secrets
+
+# Specify host key in the ipsec.secrets
+cat << EOF > /etc/ipsec.conf
+# ipsec.conf - strongSwan IPsec configuration file
+config setup
+        # strictcrlpolicy=yes
+        # uniqueids = no
+        # charondebug="cfg 2, dmn 2, ike 2, net 2"
+
+conn %default
+        ikelifetime=60m
+        keylife=30m
+        rekeymargin=3m
+        keyingtries=1
+        keyexchange=ikev2
+
+        ike=aes256gcm16-prfsha512-ecp384,aes256-sha2_512-prfsha512-ecp384,aes256-sha2_384-prfsha384-ecp384
+        esp=aes256gcm16-ecp384,aes256-sha2_512-prfsha512-ecp384
+
+conn client
+        # local: cloud
+        left=10.140.0.2 #%defaultroute
+        leftsubnet=10.140.0.2, 10.10.10.0/24
+        leftfirewall=yes
+
+        leftauth=pubkey
+        leftcert=vpn.bfs.tw.pem
+        leftid=vpn.bfs.tw
+
+        # remote: VPN client
+        right=%any
+        # must omit: rightsubnet=...
+        rightsourceip=10.10.10.1/24
+        rightauth=pubkey
+        auto=add
+EOF
