14e5a23fa895cfc127ce84639f421c4670944727
bookgin <dongsheoil@gmail.com>
2018-08-04 15:56:59 +0800

    Add algo vpn compatible gen pkcs12 script

--- gen-pkcs12.sh
+++ gen-pkcs12.sh
@@ -2,41 +2,32 @@
 
 set -e
 
-if [ `whoami` != 'root' ]; then
-  echo "You must be root"
-  exit -1
-fi
-
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 username"
-  exit -1
-fi
-
-cd /etc/ipsec.d/
-mkdir -p p12
-
-# Client cert
 name=$1
-key=private/${name}-key.pem
-cert=certs/${name}-cert.pem
-p12cert=p12/${name}.p12
-cn="C=TW, O=BFS, CN=$name"
+easyrsa_CA_password=""
+IP_subject_alt_name=""
+subjectAltName="IP:$IP_subject_alt_name"
+#subjectAltName="IP:$IP_subject_alt_name,DNS:example.com"
 
-ipsec pki --gen --type ecdsa --outform pem > $key
-chmod 600 $key
-ipsec pki --pub --in $key --type ecdsa \
-| ipsec pki --issue --lifetime 180 --outform pem \
---cacert cacerts/cacert.pem \
---cakey private/cakey.pem \
---dn "$cn" \
---san "$name" \
-> $cert
+openssl req -utf8 -new \
+-newkey ec:ecparams/secp384r1.pem \
+-config <(cat openssl.cnf <(printf "[basic_exts]\nsubjectAltName=DNS:$name")) \
+-keyout private/$name.key \
+-out reqs/$name.req -nodes \
+-passin pass:"$easyrsa_CA_password" \
+-subj "/CN=$name" -batch 
 
-# Pack to PKCS#12 file
-openssl pkcs12 -export -name "Client certificate for $name" \
--inkey $key \
--in $cert  \
--certfile cacerts/cacert.pem \
--caname "BFS Root CA" \
--out $p12cert
-chmod 600 $p12cert
+openssl ca -utf8 \
+-in reqs/$name.req \
+-out certs/$name.crt \
+-config <(cat openssl.cnf <(printf "[basic_exts]\nsubjectAltName=DNS:$name")) \
+-days 3650 -batch \
+-passin pass:"$easyrsa_CA_password" \
+-subj "/CN=$name"
+
+openssl pkcs12 \
+-in certs/$name.crt \
+-inkey private/$name.key \
+-export \
+-name $name \
+-out private/$name.p12
+#-passout pass:"{{ easyrsa_p12_export_password }}"
